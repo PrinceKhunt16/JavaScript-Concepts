172d0294f91c4a9b0bf8f4f291e878feaff653cf
Prince Khunt <princekhuntdatastructure@gmail.com>
2022-08-01 10:08:37 +0530

    Arrays Methods

---
+++ ArraysMethods/index.js
@@ -0,0 +1,43 @@
+/* 
+
+1. length
+2. reverse
+3. sort
+4. fill
+5. join
+6. toString
+7. pop
+8. shift
+9. push
+10. unshift
+11. concat
+12. spilce
+13. lastIndexOf
+14. indexOf
+15. of
+16. slice
+17. findIndex
+18. find
+19. includes
+20. reduceRight
+21. reduce
+22. isArray
+23. filter
+24. map
+25. forEach
+26. some
+27. every
+28. entries
+29. keys
+30. values
+31. from
+32. copyWithin
+33. toLocaleString
+34. flat
+35. flatMap
+36. constructor
+
+Programiz
+https://www.programiz.com/javascript/library/array
+
+*/
