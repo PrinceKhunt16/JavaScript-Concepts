d081474f634ff5f0e51edb521f6c9224e50ba6dc
Prince Khunt <princekhuntdatastructure@gmail.com>
2022-08-01 22:18:52 +0530

    String Methods

---
+++ StringMethods/index.js
@@ -0,0 +1,34 @@
+/* 
+
+1. length
+2. replace
+3. indexOf
+4. lastIndexOf
+5. startsWith
+6. endsWith
+7. toUpperCase
+8. toLowerCase
+9. includes
+10. repeat
+11. charAt
+12. charCodeAt
+13. fromCharCode
+14. substring
+15. padStart
+16. padEnd
+17. codePointAt
+18. fromCodePoint
+19. match
+20. matchAll
+21. localeCompare
+22. search
+23. replaceAll
+24. concat
+25. split
+26. trim
+27. slice
+
+Programiz
+https://www.programiz.com/javascript/library/string
+
+*/
