d01407065531fce17068d20579b9c25caa7e0608
Prince Khunt <princekhuntdatastructure@gmail.com>
2022-08-05 08:27:22 +0530

    PreventExtensions method

--- ObjectsMethods/index.js
+++ ObjectsMethods/index.js
@@ -250,3 +250,32 @@ console.log(car);
 // Check if method is seal so return true otherwise false
 
 // Code available in seal method
+
+/*
+
+9. preventExtensions
+
+*/
+
+// The Object.preventExtensions() method in JavaScript is standard built-in objects which prevents new properties from ever being added to an object.
+
+// Example: 1
+
+const movie = {
+    name: "RRR",
+    details: {
+        director: "Raja Mouli"
+    }
+};
+
+movie.song = "Nacho Nacho";
+
+Object.preventExtensions(movie);
+
+// Not add any property
+movie.time = "180min";
+
+// Delete any available property
+delete movie.song;
+
+console.log(movie);
