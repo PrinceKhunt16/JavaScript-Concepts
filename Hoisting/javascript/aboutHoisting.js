c18a8fabdf8c455f83f9060f7af363163b25e12f
Prince Khunt <princekhuntdatastructure@gmail.com>
2022-08-16 15:28:17 +0530

    Hoisting

---
+++ Hoisting/javascript/aboutHoisting.js
@@ -0,0 +1,6 @@
+/*
+
+What is Hoisting ?
+JavaScript Hoisting refers to the process whereby the interpreter appears to move the declaration of functions, variables or classes to the top of their scope, prior to execution of the code. Hoisting allows functions to be safely used in code before they are declared.
+
+*/
---
+++ Hoisting/javascript/example.js
@@ -0,0 +1,83 @@
+// 1
+
+/*
+
+console.log(n);
+var n;
+
+var m;
+console.log(m);
+
+*/
+
+// 2
+
+/*
+
+q = 5;
+console.log(q);
+var q; 
+
+var p;
+p = 5;
+console.log(p);
+
+*/
+
+// 3 
+
+/*
+
+console.log(c);
+var c = 5;
+
+var d;
+console.log(d);
+d = 5;
+
+*/
+
+// 4
+
+/*
+
+var a = 4;
+
+function hyy() {
+    b = 'hello';
+    console.log(b);
+    var b;
+}
+
+hyy();
+console.log(b);
+
+*/
+
+// 5 
+
+/*
+
+a = 5;
+console.log(a);
+let a;
+
+*/
+
+// 6
+
+/*
+
+hi();
+
+function hi() {
+    console.log('Hi, there.');
+}
+
+hello();
+
+let hello = function() {
+    console.log('Hi, there.');
+}
+
+*/
