df87d6abd2bf257af2376c5735e4219f80f7c6b0
Prince Khunt <princekhuntdatastructure@gmail.com>
2022-08-21 22:41:04 +0530

    Undefined and Not Defined

---
+++ UndefinedAndNotDefined/javascript/aboutUndefinedAndNotDefined.js
@@ -0,0 +1,9 @@
+/*
+
+What is undefined in javascript ?
+A variable that has not been assigned a value is of type undefined. A method or statement also returns undefined if the variable that is being evaluated does not have an assigned value. A function returns undefined if a value was not returned.
+
+What is not defined in javascript ?
+It is one of the reference errors that javascript will throw when someone accesses the variable which is not inside the memory heap.
+
+*/
---
+++ UndefinedAndNotDefined/javascript/example.js
@@ -0,0 +1,15 @@
+// 1
+
+/*
+
+console.log(a); 
+var a = 10;
+console.log(a);
+
+*/
+
+// 2
+
+var a;
+console.log(a);
+console.log(b);
