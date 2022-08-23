bfe4cca283918a305418efad987fb50aad4c70f9
Prince Khunt <princekhuntdatastructure@gmail.com>
2022-08-23 11:32:21 +0530

    Function

---
+++ Function/javascript/aboutFunction.js
@@ -0,0 +1,27 @@
+/*
+
+What is function statement ?
+The function statement is declares a function.
+
+What is function expression ?
+A function expression can be used as an Immediately Invoked Function Expression which runs as soon as it is defined. A function expression has to be stored in a variable and can be accessed using variableName. With the ES6 features introducing Arrow Function, it becomes more easier to declare function expression.
+
+What is function declaration ?
+The function declaration (function statement) defines a function with the specified parameters. function statement and function declaration is same but You can also define functions using the Function constructor and a function expression.
+
+What is function anonymous ?
+In JavaScript, an anonymous function is that type of function that has no name or we can say which is without any name. When we create an anonymous function, it is declared without any identifier. It is the difference between a normal function and an anonymous function.
+
+What is named function expression ?
+Named functions can be either declared in a statement or used in an expression. Named function expressions create readable stack traces. The name of the function is bound inside its body, and that can be useful. And we can use the name to have a function invoke itself, or to access its properties like any other object.
+
+Named Function Expresstion
+https://stackoverflow.com/questions/15336347/why-use-named-function-expressions
+
+What is difference between parameters and arguments ?
+Function parameters are the names listed in the function's definition. Function arguments are the real values passed to the function. Parameters are initialized to the values of the arguments supplied.
+
+What is first class function ?
+A programming language is said to have First-class functions if functions in that language are treated like other variables. So the functions can be assigned to any other variable or passed as an argument or can be returned by another function. JavaScript treat function as a first-class-citizens. This means that functions are simply a value and are just another type of object.
+
+*/
---
+++ Function/javascript/example.js
@@ -0,0 +1,83 @@
+// Function Statement 
+
+function a(){
+    console.log('Function Statement');
+}
+
+a();
+
+// Function Expression
+
+var b = function(){
+    console.log('Function Expression');
+}
+
+b();
+
+// Anonymous Function
+
+/*
+
+function(){
+    console.log('Anonymous Function');
+}
+
+*/
+
+// Named Funciton Expression
+
+var c = function named(){
+    console.log(named);
+}
+
+c();
+
+var obj = {
+    x: function(){
+       console.log(typeof x);   
+       console.log(obj.x.name); 
+    },
+    y: function y(){
+       console.log(typeof y);   
+       console.log(obj.y.name); 
+    }
+};
+
+obj.x();
+obj.y();
+
+/*
+
+setTimeout(function sayMoo() {
+    console.log('Moo');
+    setTimeout(sayMoo, 10000);
+}, 1000);
+
+*/
+
+// Difference between parameters and arguments ?
+
+// parameters
+var d = function(x, y){
+    console.log(x + y);
+}
+
+// arguments
+d(10, 20);
+
+// First Class Function
+
+function func(){
+    console.log('argument function')
+}
+
+var f = function(func){
+    func();
+    
+    return function g(){
+        console.log('return function')
+    }
+}
+
+var fun = f(func);
+fun();
