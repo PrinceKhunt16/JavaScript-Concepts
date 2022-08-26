c631278bffc80fd4daaccf63fa309477dd90817f
Prince Khunt <princekhuntdatastructure@gmail.com>
2022-08-26 11:16:40 +0530

    Pre Defined Data Structure

---
+++ DataStructure/javascript/aboutDataStructure.js
@@ -0,0 +1,17 @@
+/*
+
+1. Array 
+2. Objects
+3. Sets
+4. Maps
+1. Stacks
+2. Queues
+3. Linked List
+4. Hash
+5. Trees
+6. Graphs
+
+What is data structure ?
+A data structure is a format to organize, manage and store data in a way that allows efficient access and modification. More precisely, a data structure is a collection of data values, the relationships among them, and the functions or operations that can be applied to that data.
+
+*/
