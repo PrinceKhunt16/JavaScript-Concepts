e19935a1b7ddf0263fa4adb92b487a17ac351a0d
Prince Khunt <princekhuntdatastructure@gmail.com>
2022-09-02 00:27:42 +0530

    Ajax

---
+++ Ajax/javascript/expample.js
@@ -0,0 +1,17 @@
+// 1
+
+const URL = 'https://jsonplaceholder.typicode.com/posts';
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', URL);
+xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4){
+        const response = xhr.response;
+        const data = JSON.parse(response);
+        console.log(data);
+    }
+}
+xhr.onerror = () => {
+    console.log('network error');
+}
+xhr.send();
